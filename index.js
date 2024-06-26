be3b413fe1e62db63fc62a582ee9670f25fdd89c
NestTiga <nestortigasi371@gmail.com>
2024-06-26 17:05:49 -0500

    Conexión a la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { pool } from './db.js';
 
 const app = express();
 
+app.get('/pacientes', async (req, res) => {
+    const [result]= await pool.query('SELECT * FROM pacientes')
+    res.json(result);
+});
 
 app.listen(3000, () => {    
     console.log('Servidor escuchando en el puerto 3000');
